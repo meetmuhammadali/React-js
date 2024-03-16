c94177321cfce58ff9995ee1d93039d19b82e6bc
meetmuhammadali <bdomuhammadali@gmail.com>
2024-03-16 10:17:27 +0500

    Multi-Faq-Slection

---
+++ src/Components/accordian/data.js
@@ -0,0 +1,16 @@
+const data = [
+    { id: '1',
+      question: "What are accordion Component?",
+      answer: "Accordion components are user interface elements used for organize"
+    },
+    { id: '2',
+      question: "What are accord Component?",
+      answer: "Accordion components are user interface elements used for organize"
+    },
+    { id: '3',
+      question: "What are cordion Component?",
+      answer: "Accordion components are user interface elements used for organize"
+    }
+];
+
+export default data;
---
+++ src/Components/accordian/index.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import data from "./data";
+import  './style.css';
+
+export default function Accordian(){
+    const[selected, setSelected] = useState(null);
+    const [enableMultiSection, setEnableNultiSection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+function handleSingleSelection(getCurrentId){
+  setSelected(getCurrentId === selected ? null : getCurrentId)
+}
+
+function handleMultiSelection(getCurrentId){
+    let cpyMutiple = [...multiple];
+    const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId)
+    console.log(findIndexOfCurrentId);
+    if(findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId)
+    else cpyMutiple.splice(findIndexOfCurrentId, 1)
+
+    setMultiple(cpyMutiple)
+}
+
+console.log(selected, multiple);
+    return <div className="wrapper">
+        <button onClick={()=> setEnableNultiSection(!enableMultiSection)}>Enable Multi Section</button>
+        <div className="accordian">
+           {
+               data && data.length > 0 ?
+               data.map(dataItem=> <div className="item">
+                <div 
+                onClick={ 
+                    enableMultiSection ? ()=> handleMultiSelection(dataItem.id) 
+                    : ()=>handleSingleSelection(dataItem.id)} 
+                    className="title">
+                    <h3>{dataItem.question}</h3>
+                    <span>+</span>
+                </div>
+                {
+                    enableMultiSection ?
+                    multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div> )
+                    : selected === dataItem.id &&  (<div className="content">{dataItem.answer}</div>
+               ) }
+                {
+                     }
+               </div>
+               )
+               : (<div>No data found!</div>
+                   ) }
+        </div>
+
+    </div>;
+
+ }
